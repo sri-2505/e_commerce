80b0aaa0d16d73115aa9890a9472a81872eecacb
Srikanth <ssssri32546@gmail.com>
2023-10-27 19:23:21 +0700

    allowed host updated. and gunicorn config file added.

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.204.147.27']
 
 
 # Application definition
