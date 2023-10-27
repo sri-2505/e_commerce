345f3a040b11a0531bdf34991d497ea18e0c7c45
Srikanth <ssssri32546@gmail.com>
2023-10-27 17:56:46 +0700

    debug false and allowed host moved to settings

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['43.204.147.27']
 
 
 # Application definition
