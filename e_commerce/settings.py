57084cc3205642534fbcbbb300cc0aba096e1e4c
Srikanth <ssssri32546@gmail.com>
2023-10-28 12:48:48 +0700

    boto package used to read the env file from the s3 in settings

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -12,8 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+import boto3
 
+# Initialize the S3 client using the IAM role credentials
+s3 = boto3.client('s3')
+
+# S3 bucket and object key where the .env file is located
+s3_bucket = 'majestic-env'
+s3_object_key = 's3://majestic-env/.env'
+
+# Download the .env file from S3
+s3.download_file(s3_bucket, s3_object_key, '.env')
+
+from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
