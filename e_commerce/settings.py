353f7e289220a4e881bea7b658b00e0281a5dd63
Srikanth <ssssri32546@gmail.com>
2023-10-30 13:53:20 +0700

    white noise added in installed

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'whitenoise.runserver_nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
