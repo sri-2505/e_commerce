9bd56f7aa5b2985edfd95f5d53a624f4de6502af
Srikanth <ssssri32546@gmail.com>
2023-10-30 14:03:13 +0700

    static root updated.

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
 
-STATIC_ROOT = 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
