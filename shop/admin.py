0a160e3c56ef328517ca1ac0e3154cd9f2b52b11
sri-2505 <86188561+sri-2505@users.noreply.github.com>
2023-10-31 15:12:51 +0700

    Revert "all changes completed"

--- shop/admin.py
+++ shop/admin.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
 from .models import Category
-from .models import SubCategory
 from .models import Product
 
 # Over writing the category model for admin only
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'image', 'description')
 
-class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image', 'description')
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'product_image', 'category_id')
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(Product, ProductAdmin)
